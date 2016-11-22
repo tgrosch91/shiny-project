a5392fbe87acd2c5d7fc742fa4458ad0d89f1258
Jarrett Pon <Jarrett@Jarretts-MacBook-Pro.local>
2016-11-22 00:51:07 -0600

    Uploaded Scrabble Challenge for Teresa.

--- web/views/scrabble_page.php
+++ web/views/scrabble_page.php
@@ -1,3 +1,3 @@
 <div class="container scrabble">
-  Scrabble App goes here.
+  Welcome to the scrabble&trade; word program that will find words for you given a list of letters. To use this program enter the list of letters you have, select which dictionary you want to pull words from, and select how many results you wish to display! Results will display by top points. For more information on scrabble: <a href="http://www.puzzlers.org/dokuwiki/doku.php?id=solving%3awordlists%3aabout%3astart">Dictionaries To use</a> <a href="http://www.scrabblepages.com/scrabble/rules/">Scrabble Rules</a>
 </div>
