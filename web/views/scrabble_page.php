afcaf0bb8021ba670302ceefc0f0c7be6b110558
tgrosch91 <tgrosch91@gmail.com>
2016-11-23 15:11:24 -0600

    added list of dictionaries

--- web/views/scrabble_page.php
+++ web/views/scrabble_page.php
@@ -1,3 +1,22 @@
 <div class="container scrabble">
-  Welcome to the scrabble&trade; word program that will find words for you given a list of letters. To use this program enter the list of letters you have, select which dictionary you want to pull words from, and select how many results you wish to display! Results will display by top points. For more information on scrabble: <a href="http://www.puzzlers.org/dokuwiki/doku.php?id=solving%3awordlists%3aabout%3astart">Dictionaries To use</a> <a href="http://www.scrabblepages.com/scrabble/rules/">Scrabble Rules</a>
+  <div class="scrabble-header">
+    <h2>Scrabble&trade; Word Helper<h2>
+    </div>
+<p>  Welcome to the scrabble&trade; word program that will find words for you given a list of letters. To use this program enter the list of letters you have, select which dictionary you want to pull words from, and select how many results you wish to display! Results will display by top points. For more information on scrabble: <a href="http://www.puzzlers.org/dokuwiki/doku.php?id=solving%3awordlists%3aabout%3astart">Dictionaries To use</a> <a href="http://www.scrabblepages.com/scrabble/rules/">Scrabble Rules</a></p>
+
+<form action="scrabble.php" method="post">
+    <div class="form-group">
+        <select id="sports-stats" name="stats[]" class="form-control" multiple>
+          <option>Official Scrabble Player's Dictionary</option>
+          <option>Enable</option>
+          <option>Webster's New Interntional</option>
+          <option>Moby Words</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <button class="btn btn-default" type="submit">Choose Dictionary</button>
+    </div>
+</form>
+
+
 </div>
