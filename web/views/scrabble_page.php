3fa0b932e110dd3b3e834d6092e243a0c07723c8
tgrosch91 <tgrosch91@gmail.com>
2016-11-27 12:50:14 -0600

    copy and pasted some stuff

--- web/views/scrabble_page.php
+++ web/views/scrabble_page.php
@@ -2,11 +2,11 @@
   <div class="scrabble-header">
     <h2>Scrabble&trade; Word Helper<h2>
     </div>
-<p>  Welcome to the scrabble&trade; word program that will find words for you given a list of letters. To use this program enter the list of letters you have, select which dictionary you want to pull words from, and select how many results you wish to display! Results will display by top points. For more information on scrabble: <a href="http://www.puzzlers.org/dokuwiki/doku.php?id=solving%3awordlists%3aabout%3astart">Dictionaries To use</a> <a href="http://www.scrabblepages.com/scrabble/rules/">Scrabble Rules</a></p>
+<p>  Welcome to the Scrabble&trade; word program that will find words for you given a list of letters. To use this program enter the list of letters you have, select which dictionary you want to pull words from, and select how many results you wish to display! Results will display by top points. For more information on scrabble: <a href="http://www.puzzlers.org/dokuwiki/doku.php?id=solving%3awordlists%3aabout%3astart">Dictionaries To Use</a> <a href="http://www.scrabblepages.com/scrabble/rules/">Scrabble Rules</a></p>
 
 <form action="scrabble.php" method="post">
     <div class="form-group">
-        <select id="sports-stats" name="stats[]" class="form-control" multiple>
+        <select id="dictionary-choices" name="stats[]" class="form-control" multiple>
           <option>Official Scrabble Player's Dictionary</option>
           <option>Enable</option>
           <option>Webster's New Interntional</option>
