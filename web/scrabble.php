92bd6fe513e6aa8abae63cea544c26a7a5daebc3
tgrosch91 <tgrosch91@gmail.com>
2016-11-28 23:33:11 -0600

    getting close to specialized sorting mechanism

--- web/scrabble.php
+++ web/scrabble.php
@@ -19,6 +19,8 @@
       sort($selectedLettersArray);
       $selectedLettersString = implode($selectedLettersArray);
 
+      $letterValues = [1 => ["a","e","i","l","n","o","r","s","t","u"], 2 => ["d","g"], 3 => ["b","c","m","p"], 4 => ["f","h","v","w","y"], 5 => ["k"], 8 => ["j","x"], 10 => ["q","z"]];
+
 
       foreach ($csvDic as &$value){
 
@@ -35,6 +37,35 @@
         return $value;
       }
 
+      function sort_by_tile_value($letterValues, $letterArray){
+        $size = count($letterArray);
+        for ($i=0; $i<$size; $i++) {
+          for ($j=0; $j<$size-1-$i; $j++) {
+            $first=$letterArray[$j];
+            //right now this isn't working because array_keys is just searching for first, not the value that contains first
+            //may need to switch up how i'v organized letter_values
+            $first_value = array_keys($letterValues, $first);
+            $second_value = array_values(array_keys($letterValues, $letterArray[$j+1]));
+            var_dump($first); die;
+            if ($second_value < $first_value) {
+              swap($letterArray, $first_value, $second_value);
+            }
+          }
+        }
+        return $letterArray;
+      }
+
+      function swap(&$arr, $a, $b) {
+        $tmp = $arr[$a];
+        $arr[$a] = $arr[$b];
+        $arr[$b] = $tmp;
+        }
+
+      $test_array = ["a","z","d"];
+
+      $testResult = sort_by_tile_value($letterValues, $test_array);
+      var_dump($testResult); die;
+
 
       $csvDicSorted = [];
 
@@ -44,15 +75,14 @@
       }
 
       function add_to_possibilities($dictionary, $allLetters){
-        $matchArrayKeys = []
+        $matchArrayKeys = [];
 
       }
 
       $matchArrayKeys = array_keys($csvDicSorted, $selectedLettersString);
 
-      $letterValues = [1 => ["a","e","i","l","n","o","r","s","t","u"], 2 => ["d","g"], 3 => ["b","c","m","p"], 4 => ["f","h","v","w","y"], 5 => ["k"], 8 => ["j","x"], 10 => ["q","z"]];
 
-      var_dump($letterValues); die;
+      //var_dump($letterValues); die;
 
 
 
